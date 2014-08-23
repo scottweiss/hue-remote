0438c12c3b104de8572285dbafa6919806a8ec5f
Scott Weiss <scott.a.weiss@gmail.com>
2014-08-23 12:09:07 -0400

    sdf

--- js/api.js
+++ js/api.js
@@ -91,6 +91,14 @@ function lightsOn() {
         Origin: 'http://sttwss.com',
         contentType: "application/json; charset=utf-8",
         url: '//10.0.0.40/api/scottweiss/groups/0/action',
+          xhrFields: {
+    // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+    // This can be used to set the 'withCredentials' property.
+    // Set the value to 'true' if you'd like to pass cookies to the server.
+    // If this is enabled, your server must respond with the header
+    // 'Access-Control-Allow-Credentials: true'.
+    withCredentials: false
+  },
 
         data: JSON.stringify(DataToSend),
         dataType: "json",
